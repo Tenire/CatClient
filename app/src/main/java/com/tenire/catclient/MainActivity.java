b34ed18676c6e0d738ab94368d04166bc4ff3381
Tenire <i@tenire.com>
2021-09-14 22:10:14 +0800

    1 commit for 1.0.1
    初步写了一个外壳

---
+++ app/src/main/java/com/tenire/catclient/MainActivity.java
@@ -0,0 +1,99 @@
+package com.tenire.catclient;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    ArrayList<View> list;
+
+    Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = getList();
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.main_viewpager);
+
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter();
+
+        viewPager.setAdapter(myPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+    }
+
+    public ArrayList<View> getList() {
+        list = new ArrayList<View>();
+        LinearLayout linearLayout = (LinearLayout)getLayoutInflater().inflate(R.layout.viewpager_item, null);
+        list.add(linearLayout);
+
+        linearLayout = (LinearLayout)getLayoutInflater().inflate(R.layout.viewpager_item, null);
+        list.add(linearLayout);
+        return list;
+    }
+
+
+    class MyPagerAdapter extends PagerAdapter
+    {
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+            return view == o;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            container.addView(list.get(position));
+            return list.get(position);
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView(list.get(position));
+        }
+    }
+}
